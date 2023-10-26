1ab1cc755723f6cedc53200a9741d2aa036108b1
Rishabh Singh <52191143+rishabh0085@users.noreply.github.com>
2023-10-27 00:02:31 +0530

    Add files via upload

---
+++ WebCrawler/src/main/java/org/example/DatabaseConnection.java
@@ -0,0 +1,39 @@
+package org.example;
+
+import java.io.FileNotFoundException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    static Connection connection = null;
+    public static Connection getConnection()
+    {
+        if(connection!=null)
+            return connection;
+
+        String user = "root";
+        String pwd = "Ri$habh14";
+        String db = "searchengineapp";
+        return getConnection(user, pwd, db);
+    }
+
+    private static Connection getConnection( String user, String pwd, String db)
+    {
+        try
+        {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost/searchengineapp","root","Ri$habh14");
+
+        }
+        catch (ClassNotFoundException classNotFoundException)
+        {
+            classNotFoundException.printStackTrace();
+        }
+        catch (SQLException sqlException)
+        {
+            sqlException.printStackTrace();
+        }
+        return connection;
+    }
+}
